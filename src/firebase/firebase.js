5bc0f8e9757f2fc515c93eae13a9033e97e14e5c
Admin <admin@Admins-MacBook-Pro.local>
2023-11-13 18:53:47 -0800

    Hide firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmaqhmxKdfCwls17GHgFC4JcoCMgJI194",
-  authDomain: "react-345d5.firebaseapp.com",
-  projectId: "react-345d5",
-  storageBucket: "react-345d5.appspot.com",
-  messagingSenderId: "251432025971",
-  appId: "1:251432025971:web:5739d5b17a9aaf67ce0f06",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
